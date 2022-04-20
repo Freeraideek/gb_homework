9a15186ddda1658114b327f9c989cc34cb3e582a
FreeK <andryushka_99@mail.ru>
2022-04-20 13:18:55 +0300

    Создали программу но забыл комитить в процессе

--- Desktop/gb_homework/Control_work/Program.cs
+++ Desktop/gb_homework/Control_work/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программма для создания массива из строк, длинна которых меньше либо равна 3 символа.
+
+namespace project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Программист", "это", "не", "просто", "професия", "а", "призвание", "но", "им", "все", "равно", "может", "стать", "каждый" };
+            var arrayOne = new string[array.Length];
+            var size = 0;
+
+            foreach (var value in array)
+            {
+                if (value.Length <= 3)
+                {
+                    arrayOne[size] = value;
+                    size++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, arrayOne, 0, size));
+        }
+    }
+}
